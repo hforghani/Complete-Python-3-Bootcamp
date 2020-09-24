ac01c60622081df765d5468e178375632d42cfd7
Sarvar_Rahimi <sarvarrahimi111@gmail.com>
2020-09-24 16:44:37 +0330

    this is privios exersise

---
+++ sarvar_edite.py
@@ -0,0 +1,10 @@
+def remove_lowercase(string1, string2):
+    str1 = open(string1)
+    str2 = open(string2,'w')
+    for line in str1.readlines():
+        if line[0] != line[0].lower():
+            str2.write(line)
+    str2.close()
+    print(open(string2).read())
+
+remove_lowercase('sample.txt', 'sample2.txt')
